56be5c720ce9cfe986345e1ffb7d1a4664bf429d
Paul Dodd <pdodd@umich.edu>
2018-02-15 16:27:10 -0500

    added comment

--- src/chull.h
+++ src/chull.h
@@ -337,8 +337,7 @@ inline VectorRn<P,D> getOutwardNormal(const std::vector< VectorRn<P,D> >& points
 {
     // const std::vector<unsigned int>& face = faces[faceid];
     unsigned int d = dim(inside_point).value;
-    std::cout << "dim = " << d << " face size = " << face.size() << std::endl;
-    assert(d <= face.size());
+    assert(d <= face.size()); // TODO: throw error? 
     Eigen::Matrix<P, D, D> facet(d,d);
     slicen(points, face, d, facet);
     VectorRn<P,D> di = inside_point - points[face[0]];
